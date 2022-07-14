d214744df1b3ee949340019f31828f9b13d49bc3
Zachary Flota <zflota1@gmail.com>
2022-07-13 20:55:38 -0400

    created the pet blueprint and view

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+#factory
 def create_app():
     app = Flask(__name__)
 
@@ -7,5 +8,9 @@ def create_app():
     def hello():
         return 'Hello, PetFax!'
 
+    #register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    #return the app
     return app
-    
